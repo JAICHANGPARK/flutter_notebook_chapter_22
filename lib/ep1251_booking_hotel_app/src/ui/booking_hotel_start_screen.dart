7b83b9485505ab3e2a7e94e5b433e043ab4dd03b
Dreamwalker <aristojeff@gmail.com>
2022-08-22 17:38:08 +0900

    Create booking_hotel_start_screen.dart

---
+++ lib/ep1251_booking_hotel_app/src/ui/booking_hotel_start_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+
+class BookingHotelStartScreen extends StatelessWidget {
+  const BookingHotelStartScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Positioned(child: Container())
+        ],
+      ),
+    );
+  }
+}
