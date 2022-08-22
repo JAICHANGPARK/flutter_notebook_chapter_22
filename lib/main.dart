918e7d18bd05ebd2d3530fc20b4519e3045e5149
Dreamwalker <aristojeff@gmail.com>
2022-08-22 17:35:54 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,2 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_chapter_22/ep1251_booking_hotel_app/booking_hotel_app.dart';
 
+main() {
+  runApp(const BookingHotelApp());
+}
