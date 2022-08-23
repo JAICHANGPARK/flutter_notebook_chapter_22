49bf4ce67a927093df771ab01b200d142e1055ba
Dreamwalker <aristojeff@gmail.com>
2022-08-23 17:58:05 +0900

    Create booking_hotel_main_page.dart

---
+++ lib/ep1251_booking_hotel_app/src/ui/booking_hotel_main_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class BookingHotelMainPage extends StatelessWidget {
+  const BookingHotelMainPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
