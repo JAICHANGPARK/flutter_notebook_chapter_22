53574ef6f72a0495d71a23b8e13f85dc89f1b2da
Dreamwalker <aristojeff@gmail.com>
2022-08-25 17:19:18 +0900

    Create task_mng_home_page.dart

---
+++ lib/ep1254_task_mng_app/src/ui/task_mng_home_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TaskMngHomePage extends StatefulWidget {
+  const TaskMngHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<TaskMngHomePage> createState() => _TaskMngHomePageState();
+}
+
+class _TaskMngHomePageState extends State<TaskMngHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
