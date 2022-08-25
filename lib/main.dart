8496186b2d04c5a50aa4fd8a57c937a6f8d5aa5f
Dreamwalker <aristojeff@gmail.com>
2022-08-25 17:21:33 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_notebook_chapter_22/ep1251_booking_hotel_app/booking_hotel_app.dart';
+import 'package:flutter_notebook_chapter_22/ep1254_task_mng_app/task_mng_mobile_app.dart';
 
 main() {
-  runApp(const BookingHotelApp());
+  runApp(const TaskMngMobileApp());
 }
