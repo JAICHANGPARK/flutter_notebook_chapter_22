c3b85a6ee663b489a962d76c018f2c44c51dfbd2
Dreamwalker <aristojeff@gmail.com>
2022-08-26 17:29:49 +0900

    Update task_detail_page.dart

--- lib/ep1254_task_mng_app/src/ui/task_detail_page.dart
+++ lib/ep1254_task_mng_app/src/ui/task_detail_page.dart
@@ -19,13 +19,14 @@ class _TaskDetailPageState extends State<TaskDetailPage> {
             Container(
               color: Colors.white,
               child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Padding(
                     padding: const EdgeInsets.fromLTRB(16, 16, 0, 16),
                     child: Text("Landing Page Project Meeting"),
                   ),
                   Padding(
-                    padding: const EdgeInsets.fromLTRB(16, 0, 0, 0),
+                    padding: const EdgeInsets.fromLTRB(16, 0, 0, 24),
                     child: Column(
                       children: [
                         Row(
@@ -33,13 +34,17 @@ class _TaskDetailPageState extends State<TaskDetailPage> {
                             SizedBox(width: 100, child: Text("Participant")),
                           ],
                         ),
-                        SizedBox(height: 12,),
+                        SizedBox(
+                          height: 12,
+                        ),
                         Row(
                           children: [
                             SizedBox(width: 100, child: Text("Date")),
                           ],
                         ),
-                        SizedBox(height: 12,),
+                        SizedBox(
+                          height: 12,
+                        ),
                         Row(
                           children: [
                             SizedBox(width: 100, child: Text("Tags")),
@@ -48,7 +53,58 @@ class _TaskDetailPageState extends State<TaskDetailPage> {
                       ],
                     ),
                   ),
-
+                  Container(
+                    height: 38,
+                    color: Colors.red,
+                    child: Stack(
+                      children: [
+                        Positioned(
+                          bottom: 0,
+                          left: 0,
+                          right: 0,
+                          child: Divider(
+                            height: 0,
+                            color: Colors.grey,
+                          ),
+                        ),
+                        Positioned(
+                            left: 16,
+                            right: 0,
+                            top: 0,
+                            bottom: 0,
+                            child: ListView(
+                              scrollDirection: Axis.horizontal,
+                              children: [
+                                Column(
+                                  children: [
+                                    Row(
+                                      children: [
+                                        Text("Comments"),
+                                        SizedBox(
+                                          width: 4,
+                                        ),
+                                        Container(
+                                          decoration: BoxDecoration(
+                                            color: Colors.black,
+                                            borderRadius: BorderRadius.circular(2),
+                                          ),
+                                          padding: EdgeInsets.all(4),
+                                          child: Center(
+                                            child: Text("4"),
+                                          ),
+                                        )
+                                      ],
+                                    ),
+                                  ],
+                                ),
+                                Divider(
+                                  color: Colors.black,
+                                )
+                              ],
+                            ))
+                      ],
+                    ),
+                  ),
                 ],
               ),
             )
