d361c3dc71cf645cbc40ca48ee5667d10f7d952b
Dreamwalker <aristojeff@gmail.com>
2022-08-26 17:21:27 +0900

    Create task_detail_page.dart

---
+++ lib/ep1254_task_mng_app/src/ui/task_detail_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class TaskDetailPage extends StatefulWidget {
+  const TaskDetailPage({Key? key}) : super(key: key);
+
+  @override
+  State<TaskDetailPage> createState() => _TaskDetailPageState();
+}
+
+class _TaskDetailPageState extends State<TaskDetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
