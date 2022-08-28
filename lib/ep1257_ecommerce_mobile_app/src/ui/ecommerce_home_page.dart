7d7712d0232b0416b7a18dd6cf07e39ef65a9051
Dreamwalker <aristojeff@gmail.com>
2022-08-28 17:32:03 +0900

    Update ecommerce_home_page.dart

--- lib/ep1257_ecommerce_mobile_app/src/ui/ecommerce_home_page.dart
+++ lib/ep1257_ecommerce_mobile_app/src/ui/ecommerce_home_page.dart
@@ -24,24 +24,44 @@ class _EcommerceHomePageState extends State<EcommerceHomePage> {
                 child: Stack(
                   children: [
                     Positioned(
+                      right: 0,
+                      bottom: 0,
+                      left: 0,
+                      top: 16,
+                      child: Container(
+                        color: Colors.white,
+                        child: Row(
+                          children: [
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Icon(Icons.home),
+                                SizedBox(
+                                  height: 8,
+                                ),
+                                Text("Home"),
+                              ],
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                    Positioned(
                       left: 0,
                       right: 0,
                       top: 0,
                       child: CircleAvatar(
                         radius: 28,
                         backgroundColor: Colors.white,
+                        child: Container(
+                          margin: EdgeInsets.all(6),
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Colors.indigo,
+                          ),
+                        ),
                       ),
                     ),
-                    Positioned(
-                        right: 0,
-                        bottom: 0,
-                        left: 0,
-                        top: 12,
-                        child: Container(
-                          color: Colors.white,
-                          child: Row(
-
-                    ),))
                   ],
                 ),
               ),
