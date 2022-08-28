0ecdbe4f90366e0ecd7817ace87edfcd681e40b3
Dreamwalker <aristojeff@gmail.com>
2022-08-28 17:27:18 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_notebook_chapter_22/ep1254_task_mng_app/task_mng_mobile_app.dart';
 import 'package:flutter_notebook_chapter_22/ep1257_ecommerce_mobile_app/ecommerce_mobile_app.dart';
 
 main() {
