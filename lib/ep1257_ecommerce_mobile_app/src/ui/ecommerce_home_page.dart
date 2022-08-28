f266b839f3422a5afb817353993af1523a363c7f
Dreamwalker <aristojeff@gmail.com>
2022-08-28 17:23:50 +0900

    Create ecommerce_home_page.dart

---
+++ lib/ep1257_ecommerce_mobile_app/src/ui/ecommerce_home_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class EcommerceHomePage extends StatefulWidget {
+  const EcommerceHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<EcommerceHomePage> createState() => _EcommerceHomePageState();
+}
+
+class _EcommerceHomePageState extends State<EcommerceHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
