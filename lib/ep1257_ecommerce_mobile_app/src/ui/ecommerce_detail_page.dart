782c0bcfcf08b1a4ff626d7964c94b2932878726
Dreamwalker <aristojeff@gmail.com>
2022-08-31 17:15:42 +0900

    Create ecommerce_detail_page.dart

---
+++ lib/ep1257_ecommerce_mobile_app/src/ui/ecommerce_detail_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class EcommerceDetailPage extends StatefulWidget {
+  const EcommerceDetailPage({Key? key}) : super(key: key);
+
+  @override
+  State<EcommerceDetailPage> createState() => _EcommerceDetailPageState();
+}
+
+class _EcommerceDetailPageState extends State<EcommerceDetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
