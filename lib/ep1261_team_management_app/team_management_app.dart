3e32b101c84d8433a2512fe019a58c23c514a613
Dreamwalker <aristojeff@gmail.com>
2022-09-01 22:59:30 +0900

    Create team_management_app.dart

---
+++ lib/ep1261_team_management_app/team_management_app.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+
+class TeamManagementApp extends StatelessWidget {
+  const TeamManagementApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+
+    );
+  }
+}
