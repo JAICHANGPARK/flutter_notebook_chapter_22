f016d97428cd83b2c27ee36817faee5aaafe0bc7
Dreamwalker <aristojeff@gmail.com>
2022-09-01 23:03:15 +0900

    Update team_management_home_page.dart

--- lib/ep1261_team_management_app/src/ui/team_management_home_page.dart
+++ lib/ep1261_team_management_app/src/ui/team_management_home_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class TeamManagementHomePage extends StatefulWidget {
   const TeamManagementHomePage({Key? key}) : super(key: key);
 
@@ -11,6 +10,56 @@ class TeamManagementHomePage extends StatefulWidget {
 class _TeamManagementHomePageState extends State<TeamManagementHomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Row(
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text("Good morning"),
+                      SizedBox(
+                        height: 4,
+                      ),
+                      Text(
+                        "Dream Lab",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Spacer(),
+                  Container(
+                    height: 48,
+                    width: 48,
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.grey[400]!),
+                      shape: BoxShape.circle,
+                    ),
+                  ),
+                  SizedBox(
+                    width: 14,
+                  ),
+                  Container(
+                    height: 48,
+                    width: 48,
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.grey[400]!),
+                      shape: BoxShape.circle,
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
