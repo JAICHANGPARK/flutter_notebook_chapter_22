f232c7ad33026946de9e4691292cf1f4fa85de7e
Dreamwalker <aristojeff@gmail.com>
2022-09-01 22:59:27 +0900

    Create team_management_home_page.dart

---
+++ lib/ep1261_team_management_app/src/ui/team_management_home_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class TeamManagementHomePage extends StatefulWidget {
+  const TeamManagementHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<TeamManagementHomePage> createState() => _TeamManagementHomePageState();
+}
+
+class _TeamManagementHomePageState extends State<TeamManagementHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
