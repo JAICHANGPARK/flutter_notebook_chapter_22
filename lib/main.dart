eb4c2ee89d07fd51d845a855e8ea4ed57f8b5ab9
Dreamwalker <aristojeff@gmail.com>
2022-09-04 10:27:05 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_notebook_chapter_22/ep1261_team_management_app/team_management_app.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 main() {
-  runApp(const TeamManagementApp());
+  runApp(ProviderScope(child: const TeamManagementApp()));
 }
