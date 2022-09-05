da2ac3bac3d8c66d4fbc1daacff899d304368eea
Dreamwalker <aristojeff@gmail.com>
2022-09-05 18:14:31 +0900

    Update social_homa_page.dart

--- lib/ep1265_sub_basesd_social_app/src/ui/social_homa_page.dart
+++ lib/ep1265_sub_basesd_social_app/src/ui/social_homa_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class SocialHomePage extends StatefulWidget {
   const SocialHomePage({Key? key}) : super(key: key);
 
@@ -11,6 +10,18 @@ class SocialHomePage extends StatefulWidget {
 class _SocialHomePageState extends State<SocialHomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Column(
+        children: [
+          Divider(
+            height: 0,
+            color: Colors.grey[300],
+          ),
+          Container(
+            height: 72,
+          )
+        ],
+      ),
+    );
   }
 }
