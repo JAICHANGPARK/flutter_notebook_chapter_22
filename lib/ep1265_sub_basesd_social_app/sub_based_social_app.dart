1b5d6744efd76b0e75974b0519d04d738d188c86
Dreamwalker <aristojeff@gmail.com>
2022-09-05 18:13:46 +0900

    Update sub_based_social_app.dart

--- lib/ep1265_sub_basesd_social_app/sub_based_social_app.dart
+++ lib/ep1265_sub_basesd_social_app/sub_based_social_app.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_notebook_chapter_22/ep1265_sub_basesd_social_app/src/ui/social_homa_page.dart';
 
 class SubBasedSocialApp extends StatelessWidget {
   const SubBasedSocialApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return const MaterialApp(
+      home: SocialHomePage(),
+    );
   }
 }
