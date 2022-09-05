f2b51c964364c7542f5dd3cf672028de9182f426
Dreamwalker <aristojeff@gmail.com>
2022-09-05 18:13:42 +0900

    Create social_homa_page.dart

---
+++ lib/ep1265_sub_basesd_social_app/src/ui/social_homa_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class SocialHomePage extends StatefulWidget {
+  const SocialHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<SocialHomePage> createState() => _SocialHomePageState();
+}
+
+class _SocialHomePageState extends State<SocialHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
