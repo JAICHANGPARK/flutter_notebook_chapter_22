f99a9155fbe2390284aca699a30c74f887165d7b
Dreamwalker <aristojeff@gmail.com>
2022-09-05 18:11:56 +0900

    Create sub_based_social_app.dart

---
+++ lib/ep1265_sub_basesd_social_app/sub_based_social_app.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class SubBasedSocialApp extends StatelessWidget {
+  const SubBasedSocialApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
