d5a2094f862ce754a12243aeedb26e854aee5d99
Dreamwalker <aristojeff@gmail.com>
2022-09-05 18:19:22 +0900

    Update social_homa_page.dart

--- lib/ep1265_sub_basesd_social_app/src/ui/social_homa_page.dart
+++ lib/ep1265_sub_basesd_social_app/src/ui/social_homa_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class SocialHomePage extends StatefulWidget {
   const SocialHomePage({Key? key}) : super(key: key);
@@ -11,22 +12,36 @@ class _SocialHomePageState extends State<SocialHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Divider(
-            height: 0,
-            color: Colors.grey[300],
-          ),
-          Container(
-            height: 72,
-            decoration: BoxDecoration(
-              color: Colors.white,
+      body: SafeArea(
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Text(
+                  "CreateOne",
+                  style: GoogleFonts.notoSans(),
+                ),
+                Spacer(),
+                CircleAvatar(),
+                CircleAvatar(),
+              ],
             ),
-            child: Row(
-              children: [],
+            Expanded(child: Placeholder()),
+            Divider(
+              height: 0,
+              color: Colors.grey[300],
             ),
-          )
-        ],
+            Container(
+              height: 72,
+              decoration: BoxDecoration(
+                color: Colors.white,
+              ),
+              child: Row(
+                children: [],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
