be450a797a436ac76e591589974729305b98a591
Dreamwalker <aristojeff@gmail.com>
2022-09-05 18:12:30 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_notebook_chapter_22/ep1261_team_management_app/team_management_app.dart';
+import 'package:flutter_notebook_chapter_22/ep1265_sub_basesd_social_app/sub_based_social_app.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 main() {
-  runApp(ProviderScope(child: const TeamManagementApp()));
+  runApp(
+    const ProviderScope(
+      child: SubBasedSocialApp(),
+    ),
+  );
 }
