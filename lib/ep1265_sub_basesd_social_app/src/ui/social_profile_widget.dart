44ac6b32d16f71f0c9dd17ea8648d406cd1e81cf
Dreamwalker <aristojeff@gmail.com>
2022-09-07 20:34:53 +0900

    Update social_profile_widget.dart

--- lib/ep1265_sub_basesd_social_app/src/ui/social_profile_widget.dart
+++ lib/ep1265_sub_basesd_social_app/src/ui/social_profile_widget.dart
@@ -11,7 +11,66 @@ class _SocialProfilePageState extends State<SocialProfilePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: 240,
+              child: Stack(
+                children: [
+                  Positioned(
+                    left: 0,
+                    right: 0,
+                    bottom: 48,
+                    top: 0,
+                    child: Container(
+                      decoration: const BoxDecoration(
+                        color: Colors.orange,
+                        image: DecorationImage(
+                            image: NetworkImage(
+                              "https://cdn.pixabay.com/photo/2017/04/27/08/29/man-2264825__340.jpg",
+                            ),
+                            fit: BoxFit.cover),
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    left: 16,
+                    right: 16,
+                    top: 64,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: const [
+                        CircleAvatar(
+                          radius: 24,
+                          backgroundColor: Colors.white,
+                          child: Icon(Icons.keyboard_arrow_left),
+                        ),
+                        CircleAvatar(
+                          backgroundColor: Colors.white,
+                          radius: 24,
+                          child: Icon(Icons.more_vert_outlined),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const Positioned(
+                    left: 0,
+                    right: 0,
+                    bottom: 0,
+                    child: Center(
+                      child: CircleAvatar(
+                        radius: 54,
+                        backgroundColor: Colors.brown,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
