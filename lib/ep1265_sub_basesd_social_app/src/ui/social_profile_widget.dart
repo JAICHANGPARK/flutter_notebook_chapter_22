9f8dd1a3cd45cc1916b273dc568f3e0c0e6c6067
Dreamwalker <aristojeff@gmail.com>
2022-09-07 20:28:48 +0900

    Create social_profile_widget.dart

---
+++ lib/ep1265_sub_basesd_social_app/src/ui/social_profile_widget.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class SocialProfilePage extends StatefulWidget {
+  const SocialProfilePage({Key? key}) : super(key: key);
+
+  @override
+  State<SocialProfilePage> createState() => _SocialProfilePageState();
+}
+
+class _SocialProfilePageState extends State<SocialProfilePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
