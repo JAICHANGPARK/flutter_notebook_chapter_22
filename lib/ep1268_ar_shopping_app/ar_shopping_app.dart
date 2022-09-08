d9ab6c2dac2ac90ca46be18073e12ed688ef931d
Dreamwalker <aristojeff@gmail.com>
2022-09-08 17:19:49 +0900

    Update ar_shopping_app.dart

---
+++ lib/ep1268_ar_shopping_app/ar_shopping_app.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+
+class ArShoppingApp extends StatelessWidget {
+  const ArShoppingApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+
+    );
+  }
+}
