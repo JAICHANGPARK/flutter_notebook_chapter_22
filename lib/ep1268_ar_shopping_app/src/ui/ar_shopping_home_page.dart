6f00128e6dd0dbe1eb0c1f17b6b143ac8269f994
Dreamwalker <aristojeff@gmail.com>
2022-09-08 17:31:13 +0900

    Update ar_shopping_home_page.dart

--- lib/ep1268_ar_shopping_app/src/ui/ar_shopping_home_page.dart
+++ lib/ep1268_ar_shopping_app/src/ui/ar_shopping_home_page.dart
@@ -16,7 +16,7 @@ class _ArShoppingHomePageState extends State<ArShoppingHomePage> {
           children: [
             Positioned(
               left: 16,
-              bottom: 16,
+              bottom: 12,
               right: 16,
               child: Container(
                 height: 100,
@@ -37,13 +37,16 @@ class _ArShoppingHomePageState extends State<ArShoppingHomePage> {
                       children: [
                         Icon(
                           Icons.home_filled,
-                          size: 34,color: Colors.deepPurple,
+                          size: 34,
+                          color: Colors.deepPurple,
                         ),
-                        Text("HOME",
-                          style: TextStyle(
-                              color: Colors.grey,
-                              fontSize: 12
-                          ),),
+                        SizedBox(
+                          height: 4,
+                        ),
+                        Text(
+                          "HOME",
+                          style: TextStyle(color: Colors.grey, fontSize: 12),
+                        ),
                       ],
                     ),
                     Column(
@@ -52,12 +55,15 @@ class _ArShoppingHomePageState extends State<ArShoppingHomePage> {
                         Icon(
                           Icons.explore_outlined,
                           size: 34,
+                          color: Colors.grey,
                         ),
-                        Text("EXPLORE",
-                          style: TextStyle(
-                              color: Colors.grey,
-                              fontSize: 12
-                          ),),
+                        SizedBox(
+                          height: 4,
+                        ),
+                        Text(
+                          "EXPLORE",
+                          style: TextStyle(color: Colors.grey, fontSize: 12),
+                        ),
                       ],
                     ),
                     CircleAvatar(
@@ -75,13 +81,14 @@ class _ArShoppingHomePageState extends State<ArShoppingHomePage> {
                         Icon(
                           Icons.favorite_border,
                           size: 34,
+                          color: Colors.grey,
+                        ),
+                        SizedBox(
+                          height: 4,
                         ),
                         Text(
                           "WISHLIST",
-                          style: TextStyle(
-                            color: Colors.grey,
-                            fontSize: 12
-                          ),
+                          style: TextStyle(color: Colors.grey, fontSize: 12),
                         ),
                       ],
                     ),
@@ -91,13 +98,14 @@ class _ArShoppingHomePageState extends State<ArShoppingHomePage> {
                         Icon(
                           Icons.menu,
                           size: 34,
+                          color: Colors.grey,
+                        ),
+                        SizedBox(
+                          height: 4,
                         ),
                         Text(
                           "MORE",
-                          style: TextStyle(
-                            color: Colors.grey,
-                              fontSize: 12
-                          ),
+                          style: TextStyle(color: Colors.grey, fontSize: 12),
                         ),
                       ],
                     ),
