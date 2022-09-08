904570a70a599d512901471c803d6d0b543f41a6
Dreamwalker <aristojeff@gmail.com>
2022-09-08 17:19:46 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_notebook_chapter_22/ep1265_sub_basesd_social_app/sub_based_social_app.dart';
+import 'package:flutter_notebook_chapter_22/ep1268_ar_shopping_app/ar_shopping_app.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 main() {
   runApp(
     const ProviderScope(
-      child: SubBasedSocialApp(),
+      child: ArShoppingApp(),
     ),
   );
 }
