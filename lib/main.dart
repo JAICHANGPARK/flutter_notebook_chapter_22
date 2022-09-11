12b1867a61ecea06c24be018fbc642026621ffce
Dreamwalker <aristojeff@gmail.com>
2022-09-11 14:03:21 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_notebook_chapter_22/ep1268_ar_shopping_app/ar_shopping_app.dart';
+import 'package:flutter_notebook_chapter_22/ep1271_fashion_e_commerce_app/fashion_e_commerce_app.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 main() {
   runApp(
     const ProviderScope(
-      child: ArShoppingApp(),
+      child: FashionECommerceApp(),
     ),
   );
 }
