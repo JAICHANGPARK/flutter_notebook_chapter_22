54c64b2475107de3b61880e710fdcaf3327ae933
Dreamwalker <aristojeff@gmail.com>
2022-09-11 14:05:16 +0900

    Create fashion_home_page.dart

---
+++ lib/ep1271_fashion_e_commerce_app/src/ui/fashion_home_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class FashionHomePage extends StatefulWidget {
+  const FashionHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<FashionHomePage> createState() => _FashionHomePageState();
+}
+
+class _FashionHomePageState extends State<FashionHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
