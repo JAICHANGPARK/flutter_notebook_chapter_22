272dae8755453cf2a2738c6cf2f933d05aa0fd7c
Dreamwalker <aristojeff@gmail.com>
2022-09-11 14:03:24 +0900

    Create fashion_e_commerce_app.dart

---
+++ lib/ep1271_fashion_e_commerce_app/fashion_e_commerce_app.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class FashionECommerceApp extends StatelessWidget {
+  const FashionECommerceApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
