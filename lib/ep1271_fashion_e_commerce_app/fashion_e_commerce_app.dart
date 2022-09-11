2858d707c57d79c9b6b8d0a0d8853a58b60b0de9
Dreamwalker <aristojeff@gmail.com>
2022-09-11 14:05:19 +0900

    Update fashion_e_commerce_app.dart

--- lib/ep1271_fashion_e_commerce_app/fashion_e_commerce_app.dart
+++ lib/ep1271_fashion_e_commerce_app/fashion_e_commerce_app.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_notebook_chapter_22/ep1271_fashion_e_commerce_app/src/ui/fashion_home_page.dart';
 
 class FashionECommerceApp extends StatelessWidget {
   const FashionECommerceApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      home: FashionHomePage(),
+    );
   }
 }
