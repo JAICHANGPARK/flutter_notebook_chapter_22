6af89a517845fc3e0cd53e7f66c67031e51dae6c
Dreamwalker <aristojeff@gmail.com>
2022-09-11 14:10:05 +0900

    Update fashion_home_page.dart

--- lib/ep1271_fashion_e_commerce_app/src/ui/fashion_home_page.dart
+++ lib/ep1271_fashion_e_commerce_app/src/ui/fashion_home_page.dart
@@ -13,32 +13,49 @@ class _FashionHomePageState extends State<FashionHomePage> {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
-        leading: Icon(Icons.apps),
-        title: Text("Collections"),
+        leading: const Icon(Icons.apps),
+        title: const Text("Collections"),
         centerTitle: true,
         backgroundColor: Colors.white,
         foregroundColor: Colors.black,
         elevation: 0,
         actions: [
-          IconButton(onPressed: () {}, icon: Icon(Icons.notifications_none)),
-          IconButton(onPressed: () {}, icon: Icon(Icons.shopping_bag_outlined)),
+          IconButton(onPressed: () {}, icon: const Icon(Icons.notifications_none)),
+          IconButton(onPressed: () {}, icon: const Icon(Icons.shopping_bag_outlined)),
         ],
       ),
       body: SingleChildScrollView(
         child: Padding(
-          padding: const EdgeInsets.fromLTRB(16, 16,16,0),
+          padding: const EdgeInsets.fromLTRB(16, 16, 16, 0),
           child: Column(
             children: [
-                Container(
-                  margin: EdgeInsets.only(bottom: 16),
-                  height: 120,
-                  color: Colors.red[50],
-                ),
               Container(
-                margin: EdgeInsets.only(bottom: 16),
+                margin: const EdgeInsets.only(bottom: 16),
+                height: 120,
+                color: Colors.red[50],
+              ),
+              Container(
+                margin: const EdgeInsets.only(bottom: 16),
                 height: 120,
                 color: Colors.green[50],
-              )
+              ),
+              Container(
+                margin: const EdgeInsets.only(bottom: 16),
+                height: 320,
+                color: Colors.green[50],
+                child: Row(
+                  children: [
+                    Expanded(child: Placeholder()),
+                    SizedBox(width: 16,),
+                    Expanded(child: Placeholder())
+                  ],
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.only(bottom: 16),
+                height: 120,
+                color: Colors.red[50],
+              ),
             ],
           ),
         ),
