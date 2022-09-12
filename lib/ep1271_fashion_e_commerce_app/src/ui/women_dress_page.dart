d1292665d7fee0dc2ccd2f1400d5d1656d54a100
Dreamwalker <aristojeff@gmail.com>
2022-09-12 09:50:14 +0900

    Update women_dress_page.dart

--- lib/ep1271_fashion_e_commerce_app/src/ui/women_dress_page.dart
+++ lib/ep1271_fashion_e_commerce_app/src/ui/women_dress_page.dart
@@ -29,8 +29,55 @@ class _WomenDressPageState extends State<WomenDressPage> {
           IconButton(onPressed: () {}, icon: const Icon(Icons.shopping_bag_outlined)),
         ],
       ),
-      body: Column(
-        children: [],
+      body: DefaultTabController(
+        length: 6,
+        child: Column(
+          children: [
+            TabBar(labelColor: Colors.black, unselectedLabelColor: Colors.grey,
+                indicatorColor: Colors.black,
+                tabs: [
+              Tab(
+                text: "Basic",
+              ),
+              Tab(
+                text: "Cocktail",
+              ),
+              Tab(
+                text: "Evening Gown",
+              ),
+              Tab(
+                text: "Babydoll",
+              ),
+              Tab(
+                text: "Maxi",
+              ),
+              Tab(
+                text: "Slip",
+              ),
+            ]),
+            Expanded(
+                child: TabBarView(
+              children: [
+                Container(),
+                Center(
+                  child: Text("2"),
+                ),
+                Center(
+                  child: Text("3"),
+                ),
+                Center(
+                  child: Text("4"),
+                ),
+                Center(
+                  child: Text("5"),
+                ),
+                Center(
+                  child: Text("6"),
+                )
+              ],
+            ))
+          ],
+        ),
       ),
       bottomNavigationBar: BottomAppBar(
         child: SizedBox(
