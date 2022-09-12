7b07e7a1d15e7c053576359c0e064d953a622291
Dreamwalker <aristojeff@gmail.com>
2022-09-12 09:39:47 +0900

    Create women_dress_page.dart

---
+++ lib/ep1271_fashion_e_commerce_app/src/ui/women_dress_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class WomenDressPage extends StatefulWidget {
+  const WomenDressPage({Key? key}) : super(key: key);
+
+  @override
+  State<WomenDressPage> createState() => _WomenDressPageState();
+}
+
+class _WomenDressPageState extends State<WomenDressPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [],
+      ),
+      persistentFooterButtons: [
+
+      ],
+    );
+  }
+}
