7d7e66baa714cc32c924993a38ad7e5c664f4158
Dreamwalker <aristojeff@gmail.com>
2022-09-14 17:35:37 +0900

    Update doit_todo_app.dart

--- lib/ep1274_do_it_todo_app/doit_todo_app.dart
+++ lib/ep1274_do_it_todo_app/doit_todo_app.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 
+import 'src/ui/doit_todo_home_page.dart';
 
 class DoitTodoApp extends StatelessWidget {
   const DoitTodoApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return const MaterialApp(
+      home: DoitTodoHomePage(),
+    );
   }
 }
