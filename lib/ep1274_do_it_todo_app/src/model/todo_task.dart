cf7c0a1aec5daf0223738b5fc102e3f0dd1ea342
Dreamwalker <aristojeff@gmail.com>
2022-09-14 17:40:20 +0900

    Update todo_task.dart

--- lib/ep1274_do_it_todo_app/src/model/todo_task.dart
+++ lib/ep1274_do_it_todo_app/src/model/todo_task.dart
@@ -1,13 +1,26 @@
 
-class TodoTask{
+class TaskGroup{
+  String? groupTitle;
+  List<TodoTask>? todoTask;
+
+  TaskGroup({this.groupTitle, this.todoTask});
+}
+
+class TodoTask {
   bool? isDone;
   String? task;
   String? dateString;
+  List<SubTask> subTasks;
 
-  TodoTask({this.isDone, this.task, this.dateString});
+  TodoTask({
+    this.isDone,
+    this.task,
+    this.dateString,
+    required this.subTasks,
+  });
 }
 
-class SubTask{
+class SubTask {
   String? subTask;
   bool? isDone;
 
