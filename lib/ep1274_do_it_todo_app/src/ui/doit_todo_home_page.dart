5a149acf7351cbacb922f39c0a2f6fd86af4ccd8
Dreamwalker <aristojeff@gmail.com>
2022-09-14 17:34:17 +0900

    Create doit_todo_home_page.dart

---
+++ lib/ep1274_do_it_todo_app/src/ui/doit_todo_home_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+
+class DoitTodoHomePage extends StatefulWidget {
+  const DoitTodoHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<DoitTodoHomePage> createState() => _DoitTodoHomePageState();
+}
+
+class _DoitTodoHomePageState extends State<DoitTodoHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          
+        ],
+      ),
+    );
+  }
+}
