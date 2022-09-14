ffe29cbe21519b05af7be2fc3a64f0899dbad7c8
Dreamwalker <aristojeff@gmail.com>
2022-09-14 17:42:59 +0900

    Create todo_data.dart

---
+++ lib/ep1274_do_it_todo_app/src/data/todo_data.dart
@@ -0,0 +1,56 @@
+import 'package:flutter_notebook_chapter_22/ep1274_do_it_todo_app/src/model/todo_task.dart';
+
+List<TaskGroup> taskGroupItems = [
+  TaskGroup(groupTitle: "Skincare", todoTask: [
+    TodoTask(
+      subTasks: [],
+      task: "Daily Routine",
+      dateString: "Today",
+      isDone: false,
+    ),
+    TodoTask(
+      subTasks: [],
+      task: "Deep Exfoliation",
+      dateString: "Tomorrow",
+      isDone: false,
+    ),
+    TodoTask(
+      subTasks: [],
+      task: "Brightening Mask",
+      dateString: "",
+      isDone: true,
+    ),
+    TodoTask(
+      subTasks: [],
+      task: "Scrubbing",
+      dateString: "",
+      isDone: true,
+    ),
+  ]),
+  TaskGroup(groupTitle: "Housework", todoTask: [
+    TodoTask(
+      subTasks: [],
+      task: "Daily Routine",
+      dateString: "Today",
+      isDone: false,
+    ),
+    TodoTask(
+      subTasks: [],
+      task: "Deep Exfoliation",
+      dateString: "Tomorrow",
+      isDone: false,
+    ),
+    TodoTask(
+      subTasks: [],
+      task: "Brightening Mask",
+      dateString: "",
+      isDone: true,
+    ),
+    TodoTask(
+      subTasks: [],
+      task: "Scrubbing",
+      dateString: "",
+      isDone: true,
+    ),
+  ])
+];
