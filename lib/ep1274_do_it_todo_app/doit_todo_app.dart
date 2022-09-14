a63adefd89c60379440858528d4ba52250313a9a
Dreamwalker <aristojeff@gmail.com>
2022-09-14 17:32:33 +0900

    Create doit_todo_app.dart

---
+++ lib/ep1274_do_it_todo_app/doit_todo_app.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class DoitTodoApp extends StatelessWidget {
+  const DoitTodoApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
