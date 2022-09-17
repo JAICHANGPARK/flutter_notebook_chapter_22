434cfa59e7ec0673d8064e067fd2edbd87b334e5
Dreamwalker <aristojeff@gmail.com>
2022-09-17 11:25:42 +0900

    Create article_app.dart

---
+++ lib/ep1277_article_app/article_app.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+
+class ArticleApp extends StatelessWidget {
+  const ArticleApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Articel App",
+    );
+  }
+}
