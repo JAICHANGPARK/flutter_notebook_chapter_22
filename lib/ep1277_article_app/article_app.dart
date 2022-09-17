4e0b7fc22352d32be044d251c0e7532914cf61a2
Dreamwalker <aristojeff@gmail.com>
2022-09-17 11:26:52 +0900

    Update article_app.dart

--- lib/ep1277_article_app/article_app.dart
+++ lib/ep1277_article_app/article_app.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_notebook_chapter_22/ep1277_article_app/src/ui/article_home_page.dart';
 
 class ArticleApp extends StatelessWidget {
   const ArticleApp({Key? key}) : super(key: key);
@@ -8,6 +8,7 @@ class ArticleApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Articel App",
+      home: ArticleHomePage(),
     );
   }
 }
