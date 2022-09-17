d203987f781a6bc0ff5e39b197904d4e09342c8a
Dreamwalker <aristojeff@gmail.com>
2022-09-17 11:26:41 +0900

    Create article_home_page.dart

---
+++ lib/ep1277_article_app/src/ui/article_home_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class ArticleHomePage extends StatefulWidget {
+  const ArticleHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<ArticleHomePage> createState() => _ArticleHomePageState();
+}
+
+class _ArticleHomePageState extends State<ArticleHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+
+          ],
+        ),
+      ),
+    );
+  }
+}
