27a041298a6d204cefd579ef1fa28106483798d6
Dreamwalker <aristojeff@gmail.com>
2022-09-17 11:27:13 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_notebook_chapter_22/ep1274_do_it_todo_app/doit_todo_app.dart';
+import 'package:flutter_notebook_chapter_22/ep1277_article_app/article_app.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 main() {
   runApp(
     const ProviderScope(
-      child: DoitTodoApp(),
+      child: ArticleApp(),
     ),
   );
 }
