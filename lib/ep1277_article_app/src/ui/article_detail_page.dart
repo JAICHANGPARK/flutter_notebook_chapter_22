abe70447202ce139b3069b30d949fd4de6d72a13
Dreamwalker <aristojeff@gmail.com>
2022-09-19 17:21:29 +0900

    Update article_detail_page.dart

--- lib/ep1277_article_app/src/ui/article_detail_page.dart
+++ lib/ep1277_article_app/src/ui/article_detail_page.dart
@@ -123,22 +123,25 @@ class _ArticleDetailPageState extends State<ArticleDetailPage> {
                   ),
                   Text(
                       'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'),
-                  IntrinsicHeight(
-                    child: Row(
-                      children: [
-                        Container(
-                          width: 4,
-                          height: 72,
-                          color: Colors.blueAccent,
-                        ),
-                        SizedBox(
-                          width: 16,
-                        ),
-                        Expanded(
-                          child: Text(
-                              "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. "),
-                        ),
-                      ],
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 16),
+                    child: IntrinsicHeight(
+                      child: Row(
+                        children: [
+                          Container(
+                            width: 4,
+                            height: 72,
+                            color: Colors.blueAccent,
+                          ),
+                          SizedBox(
+                            width: 16,
+                          ),
+                          Expanded(
+                            child: Text(
+                                "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. "),
+                          ),
+                        ],
+                      ),
                     ),
                   )
                 ],
