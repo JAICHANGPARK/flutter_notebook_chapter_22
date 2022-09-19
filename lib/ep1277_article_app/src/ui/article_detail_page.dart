c588fb7543bf6ac14efc0365b3522e80e561e14f
Dreamwalker <aristojeff@gmail.com>
2022-09-19 17:07:52 +0900

    Create article_detail_page.dart

---
+++ lib/ep1277_article_app/src/ui/article_detail_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class ArticleDetailPage extends StatefulWidget {
+  const ArticleDetailPage({Key? key}) : super(key: key);
+
+  @override
+  State<ArticleDetailPage> createState() => _ArticleDetailPageState();
+}
+
+class _ArticleDetailPageState extends State<ArticleDetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+
+      ),
+    );
+  }
+}
