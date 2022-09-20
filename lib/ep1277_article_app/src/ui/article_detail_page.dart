1239582cda71760e3dde91b2b2ca8d46d2b3062b
Dreamwalker <aristojeff@gmail.com>
2022-09-20 20:42:28 +0900

    Update article_detail_page.dart

--- lib/ep1277_article_app/src/ui/article_detail_page.dart
+++ lib/ep1277_article_app/src/ui/article_detail_page.dart
@@ -101,6 +101,7 @@ class _ArticleDetailPageState extends State<ArticleDetailPage> {
                       ),
                     ],
                   ),
+                  SizedBox(height: 16,),
                   Container(
                     margin: const EdgeInsets.symmetric(vertical: 16),
                     height: 160,
@@ -205,7 +206,19 @@ class _ArticleDetailPageState extends State<ArticleDetailPage> {
                     ),
                   ),
                   const Text(
-                      'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'),
+                    'Lorem ipsum dolor sit amet, consectetur adipisicing elit,'
+                    ' sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+                    ' Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi'
+                    ' ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit'
+                    ' in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur '
+                    'sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit'
+                    ' anim id est laborum.',
+                    style: TextStyle(
+                      fontSize: 12,
+                      height: 1.6,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
                 ],
               ),
             ),
