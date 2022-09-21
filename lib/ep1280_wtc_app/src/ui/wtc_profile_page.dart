f7c6ff63630e90207f8bd0f23f518bdaaaf99033
Dreamwalker <aristojeff@gmail.com>
2022-09-21 17:44:43 +0900

    Create wtc_profile_page.dart

---
+++ lib/ep1280_wtc_app/src/ui/wtc_profile_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+
+class WTCProfilePage extends StatefulWidget {
+  const WTCProfilePage({Key? key}) : super(key: key);
+
+  @override
+  State<WTCProfilePage> createState() => _WTCProfilePageState();
+}
+
+class _WTCProfilePageState extends State<WTCProfilePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          
+        ],
+      ),
+    );
+  }
+}
