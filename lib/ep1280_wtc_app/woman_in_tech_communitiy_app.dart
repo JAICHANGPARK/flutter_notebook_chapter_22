d4d73361dd51f29e69d9c279e3b6d21e4436e8ce
Dreamwalker <aristojeff@gmail.com>
2022-09-21 17:46:25 +0900

    Update woman_in_tech_communitiy_app.dart

--- lib/ep1280_wtc_app/woman_in_tech_communitiy_app.dart
+++ lib/ep1280_wtc_app/woman_in_tech_communitiy_app.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_notebook_chapter_22/ep1280_wtc_app/src/ui/wtc_profile_page.dart';
 
 class WomenInTechCommunityApp extends StatelessWidget {
   const WomenInTechCommunityApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-
+    return const MaterialApp(
+      home: WTCProfilePage(),
     );
   }
 }
