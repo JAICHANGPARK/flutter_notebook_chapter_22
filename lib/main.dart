c46f8abefcc93c14e27e06e5eb4465f6e70c48a0
Dreamwalker <aristojeff@gmail.com>
2022-09-21 17:46:58 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_notebook_chapter_22/ep1277_article_app/article_app.dart';
+import 'package:flutter_notebook_chapter_22/ep1280_wtc_app/woman_in_tech_communitiy_app.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 main() {
   runApp(
     const ProviderScope(
-      child: ArticleApp(),
+      child: WomenInTechCommunityApp(),
     ),
   );
 }
