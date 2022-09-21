387c31a6ad28defd2f74d2b4b05dc30f26e17e81
Dreamwalker <aristojeff@gmail.com>
2022-09-21 17:50:07 +0900

    Update wtc_profile_page.dart

--- lib/ep1280_wtc_app/src/ui/wtc_profile_page.dart
+++ lib/ep1280_wtc_app/src/ui/wtc_profile_page.dart
@@ -11,40 +11,51 @@ class _WTCProfilePageState extends State<WTCProfilePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(
-              right: 16,
-              top: 24,
-              left: 16,
-            ),
-            child: Row(
-              children: [
-                const Icon(Icons.arrow_back_ios_new),
-                Expanded(
-                  child: const CircleAvatar(
-                    radius: 24,
+      body: SafeArea(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(
+                right: 16,
+                top: 24,
+                left: 16,
+              ),
+              child: Row(
+                children: [
+                  const Icon(Icons.arrow_back_ios_new),
+                  SizedBox(
+                    width: 24,
                   ),
-                ),
-                Container(
-                  padding: EdgeInsets.all(8),
-                  decoration: const BoxDecoration(
-                    color: Colors.orange,
+                  Expanded(
+                    child: const CircleAvatar(
+                      radius: 24,
+                    ),
                   ),
-                  child: const Icon(
-                    Icons.add,
-                    size: 12,
-                    color: Colors.deepOrange,
+
+                  Container(
+                    padding: EdgeInsets.all(4),
+                    decoration: BoxDecoration(
+                      color: Colors.orange[200],
+                      borderRadius: BorderRadius.circular(4),
+                    ),
+                    child: const Icon(
+                      Icons.add,
+                      size: 12,
+                      color: Colors.deepOrange,
+                    ),
                   ),
-                ),
-                Icon(
-                  Icons.settings_outlined,
-                )
-              ],
-            ),
-          )
-        ],
+                  SizedBox(
+                    width: 16,
+                  ),
+                  Icon(
+                    Icons.settings_outlined,
+                    size: 28,
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
