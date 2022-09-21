aa92da6df42b1e579fc1d3a9f9dda041529385c1
Dreamwalker <aristojeff@gmail.com>
2022-09-21 17:44:41 +0900

    Create wtc_home_app.dart

---
+++ lib/ep1280_wtc_app/src/ui/wtc_home_app.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class WTCHomePage extends StatefulWidget {
+  const WTCHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<WTCHomePage> createState() => _WTCHomePageState();
+}
+
+class _WTCHomePageState extends State<WTCHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
