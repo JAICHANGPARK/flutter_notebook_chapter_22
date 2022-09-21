35ceaa0f2cd51f9fc393eb79f259d9641f686c2b
Dreamwalker <aristojeff@gmail.com>
2022-09-21 17:47:47 +0900

    Update wtc_profile_page.dart

--- lib/ep1280_wtc_app/src/ui/wtc_profile_page.dart
+++ lib/ep1280_wtc_app/src/ui/wtc_profile_page.dart
@@ -15,17 +15,23 @@ class _WTCProfilePageState extends State<WTCProfilePage> {
         children: [
           Row(
             children: [
-              Icon(Icons.arrow_back_ios_new),
-              CircleAvatar(),
+              const Icon(Icons.arrow_back_ios_new),
+              const CircleAvatar(
+                radius: 24,
+              ),
               Container(
-                decoration: BoxDecoration(
+                padding: EdgeInsets.all(8),
+                decoration: const BoxDecoration(
                   color: Colors.orange,
                 ),
-                child: Icon(
+                child: const Icon(
                   Icons.add,
                   size: 12,
                   color: Colors.deepOrange,
                 ),
+              ),
+              Icon(
+                Icons.settings_outlined,
               )
             ],
           )
