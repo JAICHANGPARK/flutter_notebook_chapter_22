2e60a054f838bd6735ae8166a776232f232765b9
Dreamwalker <aristojeff@gmail.com>
2022-09-23 17:53:24 +0900

    Update wtc_home_app.dart

--- lib/ep1280_wtc_app/src/ui/wtc_home_app.dart
+++ lib/ep1280_wtc_app/src/ui/wtc_home_app.dart
@@ -15,8 +15,68 @@ class _WTCHomePageState extends State<WTCHomePage> {
         children: [
           Expanded(child: Placeholder()),
           Container(
-            height: 160,
-          )
+            color: Colors.white,
+            height: 140,
+            margin: const EdgeInsets.only(left: 16,bottom: 8),
+            // color: Colors.pink,
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    const Text(
+                      "Dream Walker",
+                      style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15),
+                    ),
+                    const Spacer(),
+                    TextButton(
+                      style: TextButton.styleFrom(
+                        foregroundColor: Colors.grey,
+                      ),
+                      onPressed: () {},
+                      child: const Text(
+                        "See more",
+                        style: TextStyle(
+                          fontSize: 12,
+                        ),
+                      ),
+                    ),
+                    const Icon(
+                      Icons.arrow_forward,
+                      color: Colors.grey,
+                      size: 16,
+                    )
+                  ],
+                ),
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: 10,
+                    itemBuilder: (context, index) {
+                      return Padding(
+                        padding: const EdgeInsets.only(right: 16),
+                        child: Column(
+                          children: const [
+                            CircleAvatar(
+                              radius: 24,
+                            ),
+                            SizedBox(
+                              height: 6,
+                            ),
+                            Text(
+                              "Flutter",
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                              ),
+                            )
+                          ],
+                        ),
+                      );
+                    },
+                    scrollDirection: Axis.horizontal,
+                  ),
+                )
+              ],
+            ),
+          ),
         ],
       ),
     );
