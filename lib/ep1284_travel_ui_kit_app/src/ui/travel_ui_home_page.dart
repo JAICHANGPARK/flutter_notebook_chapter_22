625b08dc0f7404cdbacddb38ea0c4cfa9060e0b3
Dreamwalker <aristojeff@gmail.com>
2022-09-24 11:25:41 +0900

    Create travel_ui_home_page.dart

---
+++ lib/ep1284_travel_ui_kit_app/src/ui/travel_ui_home_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class TravelUIHomePage extends StatefulWidget {
+  const TravelUIHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<TravelUIHomePage> createState() => _TravelUIHomePageState();
+}
+
+class _TravelUIHomePageState extends State<TravelUIHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
