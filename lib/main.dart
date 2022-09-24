bdf11ec817b30c14d3e1fbb8f7594add215e2409
Dreamwalker <aristojeff@gmail.com>
2022-09-24 11:26:50 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_notebook_chapter_22/ep1280_wtc_app/woman_in_tech_communitiy_app.dart';
+import 'package:flutter_notebook_chapter_22/ep1284_travel_ui_kit_app/travel_ui_kit_app.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(
     const ProviderScope(
-      child: WomenInTechCommunityApp(),
+      child: TravelUiKitApp(),
     ),
   );
 }
