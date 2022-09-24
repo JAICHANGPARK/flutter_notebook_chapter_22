81f376d2876c0d1b8327450de4050902df412546
Dreamwalker <aristojeff@gmail.com>
2022-09-24 11:25:04 +0900

    Create travel_ui_kit_app.dart

---
+++ lib/ep1284_travel_ui_kit_app/travel_ui_kit_app.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+
+class TravelUiKitApp extends StatelessWidget {
+  const TravelUiKitApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      
+    );
+  }
+}
