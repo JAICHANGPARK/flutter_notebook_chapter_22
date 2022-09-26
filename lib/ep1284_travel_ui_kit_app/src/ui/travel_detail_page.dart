e2b6318125901f8ff10587203159e49112836c2a
Dreamwalker <aristojeff@gmail.com>
2022-09-26 16:55:56 +0900

    Create travel_detail_page.dart

---
+++ lib/ep1284_travel_ui_kit_app/src/ui/travel_detail_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class TravelUiDetailPage extends StatefulWidget {
+  const TravelUiDetailPage({Key? key}) : super(key: key);
+
+  @override
+  State<TravelUiDetailPage> createState() => _TravelUiDetailPageState();
+}
+
+class _TravelUiDetailPageState extends State<TravelUiDetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
