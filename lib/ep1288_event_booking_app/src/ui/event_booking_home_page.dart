b6e02e628bca012cef25b50fae7bd9b8cc0f2453
Dreamwalker <aristojeff@gmail.com>
2022-09-28 17:11:40 +0900

    Update event_booking_home_page.dart

--- lib/ep1288_event_booking_app/src/ui/event_booking_home_page.dart
+++ lib/ep1288_event_booking_app/src/ui/event_booking_home_page.dart
@@ -11,33 +11,62 @@ class _EventBookingHomePageState extends State<EventBookingHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
       body: SafeArea(
         child: Column(
           children: [
-            Row(
-              children: [
-                CircleAvatar(),
-                Column(
-                  children: [
-                    Text("Hi, Dreamwalker"),
-                    Row(
-                      children: [
-                        Icon(
-                          Icons.location_on_outlined,
-                          color: Colors.cyan,
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Row(
+                children: [
+                  const CircleAvatar(),
+                  SizedBox(
+                    width: 12,
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        "Hi, Dreamwalker",
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
                         ),
-                        Text("New York")
+                      ),
+                      Row(
+                        children: [
+                          const Icon(
+                            Icons.location_on_outlined,
+                            color: Colors.cyan,
+                            size: 14,
+                          ),
+                          const Text("New York")
+                        ],
+                      ),
+                    ],
+                  ),
+                  const Spacer(),
+                  SizedBox(
+                    height: 32,
+                    width: 32,
+                    child: Stack(
+                      children: [
+                        Positioned(
+                            child: IconButton(
+                          onPressed: () {},
+                          icon: const Icon(Icons.notifications_none),
+                        )),
+                        const Positioned(
+                            right: 0,
+                            top: 8,
+                            child: CircleAvatar(
+                              radius: 4,
+                              backgroundColor: Colors.red,
+                            ))
                       ],
                     ),
-                  ],
-                ),
-                Stack(
-                  children: [
-                    
-                  ],
-                )
-              ],
+                  )
+                ],
+              ),
             )
           ],
         ),
