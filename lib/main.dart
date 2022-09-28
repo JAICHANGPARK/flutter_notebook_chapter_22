005bdd6522103c25d71d368da1fc17a8738f567e
Dreamwalker <aristojeff@gmail.com>
2022-09-28 17:06:44 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_notebook_chapter_22/ep1284_travel_ui_kit_app/travel_ui_kit_app.dart';
+import 'package:flutter_notebook_chapter_22/ep1288_event_booking_app/event_booking_app.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 main() {
   WidgetsFlutterBinding.ensureInitialized();
   runApp(
     ProviderScope(
-      child: TravelUiKitApp(),
+      child: EventBookingApp(),
     ),
   );
 }
