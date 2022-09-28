13828aa8bb90722d471f647d32859bf414f331bd
Dreamwalker <aristojeff@gmail.com>
2022-09-28 17:06:46 +0900

    Update event_booking_app.dart

--- lib/ep1288_event_booking_app/event_booking_app.dart
+++ lib/ep1288_event_booking_app/event_booking_app.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_chapter_22/ep1288_event_booking_app/src/ui/event_booking_home_page.dart';
 import 'package:go_router/go_router.dart';
 
 class EventBookingApp extends StatelessWidget {
   EventBookingApp({Key? key}) : super(key: key);
   final _router = GoRouter(routes: [
-      GoRoute(path: "/",
-      builder: (context, state) =>);
-
-  ]
-
-  );
+    GoRoute(
+      path: "/",
+      builder: (context, state) => EventBookingHomePage(),
+    ),
+  ]);
 
   @override
   Widget build(BuildContext context) {
