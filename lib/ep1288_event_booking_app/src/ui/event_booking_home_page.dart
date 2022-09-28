21ae4ab0f3f2285def7dbd6662006e2aa6bf0147
Dreamwalker <aristojeff@gmail.com>
2022-09-28 17:06:29 +0900

    Create event_booking_home_page.dart

---
+++ lib/ep1288_event_booking_app/src/ui/event_booking_home_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class EventBookingHomePage extends StatefulWidget {
+  const EventBookingHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<EventBookingHomePage> createState() => _EventBookingHomePageState();
+}
+
+class _EventBookingHomePageState extends State<EventBookingHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
