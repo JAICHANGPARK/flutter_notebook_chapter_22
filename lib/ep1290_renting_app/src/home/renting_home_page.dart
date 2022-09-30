05fe974ca8a3b19d9871d1c3b48fdb7e7c04057a
Dreamwalker <aristojeff@gmail.com>
2022-10-01 08:59:23 +0900

    Update renting_home_page.dart

--- lib/ep1290_renting_app/src/home/renting_home_page.dart
+++ lib/ep1290_renting_app/src/home/renting_home_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final rentingPageIndex = StateProvider((ref) => 0);
 
 class RentingHomePage extends StatefulWidget {
   const RentingHomePage({Key? key}) : super(key: key);
@@ -10,43 +13,50 @@ class RentingHomePage extends StatefulWidget {
 class _RentingHomePageState extends State<RentingHomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: [],
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        showSelectedLabels: false,
-        showUnselectedLabels: false,
-        type: BottomNavigationBarType.shifting,
-        selectedItemColor: Color(0xff264c86),
-        unselectedItemColor: Colors.black,
-        items: [
-          BottomNavigationBarItem(
-            label: "Home",
-            icon: Icon(
-              Icons.home_filled,
+    return Consumer(builder: (context, ref, _) {
+      final index = ref.watch(rentingPageIndex);
+      return Scaffold(
+        body: Column(
+          children: [],
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          showSelectedLabels: false,
+          showUnselectedLabels: false,
+          currentIndex: index,
+          onTap: (i) {
+            ref.read(rentingPageIndex.notifier).state = i;
+          },
+          type: BottomNavigationBarType.shifting,
+          selectedItemColor: Color(0xff264c86),
+          unselectedItemColor: Colors.black,
+          items: [
+            BottomNavigationBarItem(
+              label: "Home",
+              icon: Icon(
+                Icons.home_filled,
+              ),
             ),
-          ),
-          BottomNavigationBarItem(
-            label: "Home",
-            icon: Icon(
-              Icons.favorite_border,
+            BottomNavigationBarItem(
+              label: "Home",
+              icon: Icon(
+                Icons.favorite_border,
+              ),
             ),
-          ),
-          BottomNavigationBarItem(
-            label: "Home",
-            icon: Icon(
-              Icons.notifications_active_outlined,
+            BottomNavigationBarItem(
+              label: "Home",
+              icon: Icon(
+                Icons.notifications_active_outlined,
+              ),
             ),
-          ),
-          BottomNavigationBarItem(
-            label: "Home",
-            icon: Icon(
-              Icons.perm_identity_sharp,
-            ),
-          )
-        ],
-      ),
-    );
+            BottomNavigationBarItem(
+              label: "Home",
+              icon: Icon(
+                Icons.perm_identity_sharp,
+              ),
+            )
+          ],
+        ),
+      );
+    });
   }
 }
