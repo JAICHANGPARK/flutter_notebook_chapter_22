2c7d17edc8b337c2918fa42fb2ddf23c7fbb5be0
Dreamwalker <aristojeff@gmail.com>
2022-10-01 08:51:27 +0900

    Update renting_app.dart

--- lib/ep1290_renting_app/renting_app.dart
+++ lib/ep1290_renting_app/renting_app.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_chapter_22/ep1290_renting_app/src/home/renting_home_page.dart';
 import 'package:go_router/go_router.dart';
 
 class RentingApp extends StatelessWidget {
-   RentingApp({Key? key}) : super(key: key);
+  RentingApp({Key? key}) : super(key: key);
 
-  final _router = GoRouter(routes: []);
+  final _router = GoRouter(
+    initialLocation: "/",
+    routes: [
+      GoRoute(
+        path: "/",
+        builder: (context, state) => const RentingHomePage(),
+      ),
+    ],
+  );
 
   @override
   Widget build(BuildContext context) {
