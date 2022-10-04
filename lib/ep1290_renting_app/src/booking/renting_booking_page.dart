de664811b8709c98b170f4d78f6e9ed5f96327e0
JAICHANGPARK <aristojeff@gmail.com>
2022-10-04 14:23:41 +0900

    Update renting_booking_page.dart

--- lib/ep1290_renting_app/src/booking/renting_booking_page.dart
+++ lib/ep1290_renting_app/src/booking/renting_booking_page.dart
@@ -12,18 +12,46 @@ class _RentingBookingPageState extends State<RentingBookingPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
-      body: Column(
-        children: [
-          Row(
-            children: [
-              IconButton(
-                onPressed: () {},
-                icon: Icon(Icons.arrow_back),
-                color: Colors.white,
-              )
-            ],
-          )
-        ],
+      body: SafeArea(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                vertical: 16,
+                horizontal: 16,
+              ),
+              child: Row(
+                children: [
+                  IconButton(
+                    onPressed: () {},
+                    icon: const Icon(Icons.arrow_back),
+                    color: Colors.white,
+                  ),
+                  Expanded(
+                    child: Column(
+                      children: [
+                        const Text(
+                          "Flutter House",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        const Text(
+                          "Nov, 13-16 - 3 guests",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
