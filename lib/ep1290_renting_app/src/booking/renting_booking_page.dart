5c40404443ea0631cd05cbe1a0274e3bde8d6447
JAICHANGPARK <aristojeff@gmail.com>
2022-10-04 14:19:31 +0900

    Create renting_booking_page.dart

---
+++ lib/ep1290_renting_app/src/booking/renting_booking_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class RentingBookingPage extends StatefulWidget {
+  const RentingBookingPage({Key? key}) : super(key: key);
+
+  @override
+  State<RentingBookingPage> createState() => _RentingBookingPageState();
+}
+
+class _RentingBookingPageState extends State<RentingBookingPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
