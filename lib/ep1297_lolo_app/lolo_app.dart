53ad55e9493c2b6939fed392f306c8f0b577c40f
JAICHANGPARK <aristojeff@gmail.com>
2022-10-06 23:17:46 +0900

    Update lolo_app.dart

---
+++ lib/ep1297_lolo_app/lolo_app.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class LoloApp extends StatelessWidget {
+  const LoloApp({Key? key}) : super(key: key);
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+
+    );
+  }
+}
