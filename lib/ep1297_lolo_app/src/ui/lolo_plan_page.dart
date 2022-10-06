55d6a85d3116490f21ed083fb06dd528896f7793
JAICHANGPARK <aristojeff@gmail.com>
2022-10-06 23:19:22 +0900

    Update lolo_plan_page.dart

---
+++ lib/ep1297_lolo_app/src/ui/lolo_plan_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class LoloPlanPage extends StatefulWidget {
+  const LoloPlanPage({Key? key}) : super(key: key);
+
+  @override
+  State<LoloPlanPage> createState() => _LoloPlanPageState();
+}
+
+class _LoloPlanPageState extends State<LoloPlanPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
