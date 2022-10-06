c0c7e2d04305dc3abee2115f50ee0458adf1955b
JAICHANGPARK <aristojeff@gmail.com>
2022-10-06 23:20:27 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_notebook_chapter_22/ep1290_renting_app/renting_app.dart';
+import 'package:flutter_notebook_chapter_22/ep1297_lolo_app/lolo_app.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 main() {
   WidgetsFlutterBinding.ensureInitialized();
   runApp(
     ProviderScope(
-      child: RentingApp(),
+      child: LoloApp(),
     ),
   );
 }
