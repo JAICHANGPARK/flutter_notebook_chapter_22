b93fcc18e0feea19541f82c63b21ca24066c0a0a
JAICHANGPARK <aristojeff@gmail.com>
2022-10-06 23:19:23 +0900

    Update lolo_app.dart

--- lib/ep1297_lolo_app/lolo_app.dart
+++ lib/ep1297_lolo_app/lolo_app.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_chapter_22/ep1297_lolo_app/src/ui/lolo_plan_page.dart';
 import 'package:go_router/go_router.dart';
 
 class LoloApp extends StatelessWidget {
   LoloApp({Key? key}) : super(key: key);
 
-  final _router = GoRouter(initialLocation: "/", routes: [GoRoute(path: "/")]);
+  final _router = GoRouter(initialLocation: "/", routes: [
+    GoRoute(
+      path: "/",
+      builder: (context, state) => const LoloPlanPage(),
+    )
+  ]);
 
   @override
   Widget build(BuildContext context) {
